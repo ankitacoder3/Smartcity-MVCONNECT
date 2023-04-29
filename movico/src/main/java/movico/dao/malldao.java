44e9dc99ed2e226237a0c8a011af2e31c1d5be05
ANKITA SAHOO <ankitacoder3@gmail.com>
2023-04-30 02:04:48 +0530

    Add files via upload

--- movico/src/main/java/movico/dao/malldao.java
+++ movico/src/main/java/movico/dao/malldao.java
@@ -1,3 +1,5 @@
+//DATA ACCESS OBJECT ( DAO ) FOR MALL ENTITY OR CLASS
+
 package movico.dao;
 
 import org.springframework.stereotype.Component;
@@ -11,34 +13,33 @@ import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
-public class malldao {
+public class malldao 
+{
 	@Autowired
 	private HibernateTemplate hibernateTemplate;
-	//create
+
+	//creation 
 	@Transactional
 	public void createMall(Malls mall)
-	{
-		
 		this.hibernateTemplate.saveOrUpdate(mall);
-		
-	}
-	//get all products
+ 
+	//getting all products
 	public List<Malls> getMalls()
 	{
 		List<Malls> a = this.hibernateTemplate.loadAll(Malls.class);
 		return a;
 	}
-	//delete single product
+
+	//deleting single product
 	public void deleteMall(int mid)
 	{
 		Malls k=this.hibernateTemplate.load(Malls.class, mid);
 		this.hibernateTemplate.delete(k);
 	}
-	//get single product
+
+	//getting single product
 	public Malls getMall(int mid)
-	{
 		return this.hibernateTemplate.get(Malls.class,mid);
-	}
 
 }
 
--- movico/src/main/java/movico/dao/movicodao.java
+++ movico/src/main/java/movico/dao/movicodao.java
@@ -1,3 +1,5 @@
+//DATA ACCESS OBJECT ( DAO ) 
+
 package movico.dao;
 
 import org.springframework.stereotype.Component;
@@ -11,34 +13,34 @@ import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
-public class movicodao {
+public class movicodao 
+{
 	@Autowired
 	private HibernateTemplate hibernateTemplate;
-	//create
+
+	//creating
 	@Transactional
 	public void createProduct(Product product)
-	{
-		
 		this.hibernateTemplate.saveOrUpdate(product);
-		
-	}
-	//get all products
+
+
+	//getting all products
 	public List<Product> getProducts()
 	{
 		List<Product> products = this.hibernateTemplate.loadAll(Product.class);
 		return products;
 	}
-	//delete single product
+
+	//deleting single product
 	public void deleteProduct(int pid)
 	{
 		Product p=this.hibernateTemplate.load(Product.class, pid);
 		this.hibernateTemplate.delete(p);
 	}
-	//get single product
+
+	//getting single product
 	public Product getProduct(int pid)
-	{
 		return this.hibernateTemplate.get(Product.class,pid);
-	}
 
 }
 
